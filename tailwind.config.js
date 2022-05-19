848ca2ae09ca7b6d21c933024b8a587d4317324f
Matic Jeseničnik <matic.jesenicnik@gmail.com>
2022-05-19 22:50:11 +0200

    add tailwind, clean project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/*.{html,js,tsx}",
+    "./src/**/*.{html,js,tsx}",
+    "./src/**/**/*.{html,js,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
